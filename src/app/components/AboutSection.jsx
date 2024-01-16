30167cb3e26129cb5e7ac5ffbea4ac886de0285c
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2024-01-16 10:49:22 +0530

    personal portfolio navbar about section created

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,6 +3,43 @@ import React, {useTransition, useState} from 'react'
 import Image from 'next/image';
 import TabButton from "./TabButton";
 
+const TAB_DATA = [
+    {
+        title : "Skills",
+        id : "skills",
+        content : (
+            <ul className='list-disc pl-2'>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>PostgreSQL</li>
+                <li>Sequelize</li>
+                <li>Javascript</li>
+                <li>React</li>
+            </ul>
+        )
+    },
+    {
+        title : "Education",
+        id : "education",
+        content : (
+            <ul className='list-disc pl-2'>
+                <li>Fullstack Academy of Code</li>
+                <li>University of California, Santa Cruz</li>
+            </ul>
+        )
+    },
+    {
+        title : "Certifications",
+        id : "certifications",
+        content : (
+            <ul className='list-disc pl-2'>
+                <li>AWS Cloud Practitioner</li>
+                <li>Google Professional Cloud Developer</li>
+            </ul>
+        )
+    }
+]
+
 const AboutSection = () => {
     const [tab, setTab] = useState("skills");
     const [isPending, startTransition] = useTransition();
@@ -16,8 +53,8 @@ const AboutSection = () => {
   return (
     <section className='text-white'>
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-            <img src="/images/about-image.png" width={500} height={500} alt="" />
-            <div>
+            <Image src="/images/about-image.png" width={500} height={500} alt="" />
+            <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                 <h2 className='text-4xl font-bold text-white mn-4'>About Me</h2>
                 <p className='text-base lg:text-lg'>I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
@@ -28,8 +65,10 @@ const AboutSection = () => {
             <div className="flex flex-row mt-8">
                 <TabButton selectTab={()=> handleTabChange("skills")} active={tab==="skills"}> {" "} Skills {" "} </TabButton>
                 <TabButton selectTab={()=> handleTabChange("education")} active={tab==="education"}>{" "} Education {" "} </TabButton>
-                <TabButton selectTab={()=> handleTabChange("certification")} active={tab==="certification"}>{" "} Certification {" "} </TabButton>
-                
+                <TabButton selectTab={()=> handleTabChange("certifications")} active={tab==="certifications"}>{" "} Certification {" "} </TabButton>        
+            </div>
+            <div className='mt-8'>
+                {TAB_DATA.find((t)=>t.id === tab).content}
             </div>
             </div>
         </div>
